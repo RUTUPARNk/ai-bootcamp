769e5c62d2a0832aeb39fb182f9e80635f39e82f
Don'tReadMe <avedant34@gmail.com>
2025-09-20 20:29:37 +0530

    IsSubsequence Leetcode
    Out of Daily three LCs

---
+++ daily_logs/IsSubsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        S = len(s)
+        T = len(t)
+        i, j = 0, 0
+        while i < S and j < T:
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == S
